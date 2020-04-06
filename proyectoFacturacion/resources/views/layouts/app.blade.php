a706adfcf2bf5ae05327dab22cc066ba7d9fa798
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-04-05 22:40:26 -0400

    importacion a layout

--- proyectoFacturacion/resources/views/layouts/app.blade.php
+++ proyectoFacturacion/resources/views/layouts/app.blade.php
@@ -7,15 +7,24 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap-table.min.css') }}" rel="stylesheet">
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap-table.min.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap-table-es-CL.min.js') }}" defer></script>
+    <script src="{{ asset('js/bootstrap-table-export.min.js') }}" defer></script>
+    <script src="{{ asset('js/jspdf.min.js') }}" defer></script>
+    <script src="{{ asset('js/jspdf.plugin.autotable.min.js') }}" defer></script>
+    <script src="{{ asset('js/tableExport.min.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
 </head>
 <body>
     <div id="app">
