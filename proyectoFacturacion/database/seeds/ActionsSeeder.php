cb9fcf866c21deaf8c1a6ae49261b1092882f674
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-05-05 01:43:01 -0400

    Update ActionsSeeder.php

--- proyectoFacturacion/database/seeds/ActionsSeeder.php
+++ proyectoFacturacion/database/seeds/ActionsSeeder.php
@@ -22,7 +22,16 @@ class ActionsSeeder extends Seeder
         ['actionName'=>'Administracion_delete', 'actionParentId'=>1, 'actionType'=>'Programa'], 
         ['actionName'=>'Clientes_create', 'actionParentId'=>2, 'actionType'=>'Programa'], 
         ['actionName'=>'Clientes_edit', 'actionParentId'=>2, 'actionType'=>'Programa'],
-        ['actionName'=>'Clientes_delete', 'actionParentId'=>2, 'actionType'=>'Programa'],    
+        ['actionName'=>'Clientes_delete', 'actionParentId'=>2, 'actionType'=>'Programa'], 
+        ['actionName'=>'Parametrizaciones', 'actionParentId'=>null, 'actionType'=>'Menu'],
+        ['actionName'=>'Parametrizaciones_modulos', 'actionParentId'=>12, 'actionType'=>'Menu'],
+        ['actionName'=>'Parametrizaciones_unidadesCobro', 'actionParentId'=>12, 'actionType'=>'Menu'], 
+        ['actionName'=>'Parametrizaciones_modulos_create', 'actionParentId'=>13, 'actionType'=>'Programa'],
+        ['actionName'=>'Parametrizaciones_modulos_edit', 'actionParentId'=>13, 'actionType'=>'Programa'],
+        ['actionName'=>'Parametrizaciones_modulos_delete', 'actionParentId'=>13, 'actionType'=>'Programa'], 
+        ['actionName'=>'Parametrizaciones_unidadesCobro_create', 'actionParentId'=>14, 'actionType'=>'Programa'],
+        ['actionName'=>'Parametrizaciones_unidadesCobro_edit', 'actionParentId'=>14, 'actionType'=>'Programa'],
+        ['actionName'=>'Parametrizaciones_unidadesCobro_delete', 'actionParentId'=>14, 'actionType'=>'Programa'],
 	]);
     }
 }
