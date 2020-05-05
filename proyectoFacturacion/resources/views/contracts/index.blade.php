2772466ebf76d6f53a5fd44b9e106ef08414ac7b
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-05-05 01:42:39 -0400

    Update index.blade.php

--- proyectoFacturacion/resources/views/contracts/index.blade.php
+++ proyectoFacturacion/resources/views/contracts/index.blade.php
@@ -1,11 +1,84 @@
-@extends('layouts.app')
+@extends('contracts.layout')
+@section('contractsContent')
 
-@section('content')
-<div class="card shadow">
-  <h5 class="card-header">Contratos</h5>
-  <div class="card-body">
-    <h5 class="card-title">Objetivo</h5>
-    <p class="card-text">Modulo encargado de crear, modificar y eliminar contratos.</p>
-  </div>
+<div class="row justify-content-center">
+    <div class="col-auto">
+        <div class="table-responsive">
+            <table id="tablaContracts" class="table table-hover w-auto text-nowrap" data-show-export="true"
+                data-pagination="true" data-click-to-select="true" data-show-columns="true" data-sortable="true"
+                data-search="true" data-live-search="true" data-buttons-align="left" data-search-align="right"
+                data-server-sort="false">
+                <thead>
+                    <tr>
+                        <th scope="col" data-field="ID" data-sortable="true">ID</th>
+                        <th scope="col" data-field="idClient" data-sortable="true">ID Cliente</th>
+                        <th scope="col" data-field="contractsNombre" data-sortable="true">Nombre contrato</th>
+                        <th scope="col" data-field="contractsNumero" data-sortable="true">Numero contrato</th>
+                        <th scope="col" data-field="contract_ejecutivoName" data-sortable="true">Atención</th>
+                        <th scope="col" data-field="contractsFecha" data-sortable="true">Fecha</th>
+                        <th scope="col" data-field="contractsEstado" data-sortable="true">Estado</th>
+
+                        <th scope="col" data-field="Accion" data-sortable="true">Acción</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($contracts as $contract)
+                    <tr>
+                        <td>{{$contract['id']}}</td>
+                        <td>{{$contract['contract_clientName']}}</td>
+                        <td>{{$contract['contractsNombre']}}</td>
+                        <td>{{$contract['contractsNumero']}}</td>
+                        <td>{{$contract['contract_ejecutivoName']}}</td>
+                        <td>{{$contract['contractsFecha']}}</td>
+                        <td class="text-white @if ($contract['contractsEstado'] == false) bg-secondary @else bg-success @endif">
+                            @if ($contract['contractsEstado'] == false) Inactivo
+                            @else Activo 
+                            @endif
+
+                        <td>
+                            @if(in_array(7, $authPermisos))
+                                <a class="btn btn-primary" href="{{ route('contracts.conditions', $contract['id']) }}"
+                                role="button">Condiciones</a>
+                            @endif
+
+                            @if(in_array(7, $authPermisos))
+                                <a class="btn btn-light" href="{{ route('contracts.editContractStatus', $contract['id']) }}"
+                                role="button">
+                                @if ($contract['contractsEstado'] == false) Activar
+                                @else Desactivar @endif
+                            </a>
+                            @endif
+
+                            @if(in_array(10, $authPermisos))
+                            <a class="btn btn-secondary" href="{{ route('contracts.edit', $contract['id']) }}"
+                                role="button">Editar</a>
+                            @endif
+                            @if(in_array(11, $authPermisos))
+                            <form style="display: inline-block;" action="{{ route('contracts.destroy', $contract['id']) }}"
+                                method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger" type="submit">Eliminar(DEBUG)</button>
+                            </form>
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
+
+<script>
+    //Inicializa la tabla "detalles" del dashboard
+    $('#tablaContracts').bootstrapTable({
+        pageSize: 25,
+        exportDataType: 'all',
+    });
+    document.onsubmit=function(){
+           return confirm('¿Está seguro de esta operación? Esto es irreversible.');
+       }
+
+</script>
 @endsection
