83b151bc7a5f6be773f81a7310c5e257b9c90e14
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-06-15 00:19:53 -0400

    Create layout.blade.php

---
+++ proyectoFacturacion/resources/views/billings/layout.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="card shadow">
+  <div class="card-header">
+    <ul class="nav nav-tabs card-header-tabs">
+      <li class="nav-item">
+        <a class="nav-link {{ (request()->is('billings')) ? 'active' : '' }}" href="{{route('billings.index', 0)}}">Lista de documentos tributarios</a>
+      </li>
+    </ul>
+  </div>
+
+  <div class="card-body py-3 my-3">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          @yield('billingsContent')
+        </div>
+      </div>
+    </div>  
+
+
+  </div>
+</div>
+@endsection
