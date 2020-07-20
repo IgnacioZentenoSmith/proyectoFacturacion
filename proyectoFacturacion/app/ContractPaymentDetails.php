80ed2357b396d828e5043bd891fc6a95562acfa7
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-07-20 12:17:10 -0400

    tabla detalles de pago

---
+++ proyectoFacturacion/app/ContractPaymentDetails.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ContractPaymentDetails extends Model
+{
+    protected $table = 'contract_payment_details';
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+        'idPaymentUnit',
+        'idClient',
+        'idContract',
+        'contractPaymentDetails_period',
+        'ccontractPaymentDetails_quantity',
+        'contractPaymentDetails_description',
+        'contractPaymentDetails_recepcionMunicipal',
+    ];
+
+  public function paymentUnit(){
+    return $this->hasOne('App\PaymentUnits', 'idPaymentUnit', 'id');
+  }
+  public function client(){
+    return $this->hasOne('App\Client', 'idClient', 'id');
+  }
+  public function contract(){
+    return $this->hasOne('App\Contracts', 'idContract', 'id');
+  }
+}
