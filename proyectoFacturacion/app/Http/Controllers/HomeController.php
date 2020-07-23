9bbe2e879bad0b4a03cbc09384ac5769eac91475
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-07-23 16:22:55 -0400

    APIs

--- proyectoFacturacion/app/Http/Controllers/HomeController.php
+++ proyectoFacturacion/app/Http/Controllers/HomeController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Permission;
+use App\ContractPaymentDetails;
 use Auth;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class HomeController extends Controller
 {
@@ -30,5 +32,31 @@ class HomeController extends Controller
         $authPermisos = Permission::where('idUser', $userId)->get();
         $authPermisos = $authPermisos->pluck('idActions')->toArray();
         return view('home.index', compact('authPermisos'));
+    }
+
+    public function GCI_Api() {
+        $response = Http::withHeaders([
+            'api_key' => '1234567890',
+        ])->get('https://comercialinmobiliarias.cl/gci_desarrollo/wpenaloza/api/');
+
+        return $response->json();
+    }
+    public function PVI_Api() {
+        $response = Http::withHeaders([
+            'api_key' => '6b4c17219b48f933cc4c7caf69226d46e2b91ffd',
+        ])->get('http://pvi.cl/facturacion/test.php');
+        return $response->json();
+    }
+    public function ETDTP_Api() {
+        $response = Http::withHeaders([
+            'key' => '35328fcd1b8cf9e101fc0e398de0be08',
+        ])->get('https://www.pok.cl/apirest/post.php');
+        return $response->json();
+    }
+    public function LICITA_Api() {
+        $response = Http::withHeaders([
+            'key' => '3d524a53c110e4c22463b10ed32cef9d',
+        ])->get('http://10.33.51.203/v3/restapi_licita_v1/post.php');
+        return $response->json();
     }
 }
