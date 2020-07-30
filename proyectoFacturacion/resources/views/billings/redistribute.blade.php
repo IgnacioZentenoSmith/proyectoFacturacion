66e57b32d86072e04b629457b9fe07daadb7114f
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2020-07-30 11:02:11 -0400

    Redistribuciones de unidades de pago y descuentos

--- proyectoFacturacion/resources/views/billings/redistribute.blade.php
+++ proyectoFacturacion/resources/views/billings/redistribute.blade.php
@@ -27,8 +27,9 @@
                             de unidades de pago</th>
                         <th scope="col" data-field="tributarydetails_paymentPercentage" data-sortable="true">Porcentaje
                         </th>
-                        <th scope="col" data-field="tributarydetails_paymentValue" data-sortable="true">Monto</th>
+                        <th scope="col" data-field="tributarydetails_paymentValue" data-sortable="true">Monto subtotal</th>
                         <th scope="col" data-field="tributarydetails_discount" data-sortable="true">Descuento</th>
+                        <th scope="col" data-field="tributarydetails_paymentTotalValue" data-sortable="true">Monto total</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -75,6 +76,13 @@
                             class="form-control" name="tributarydetails_discount[]" step="0.001"
                             value="{{$tributaryDetail['tributarydetails_discount']}}">
                         </td>
+
+                        <td>
+                            <input id="tributarydetails_paymentTotalValue[{{$tributaryDetail['id']}}]" type="number" onchange="getMontoTotal(this);"
+                            class="form-control" name="tributarydetails_paymentTotalValue[]" step="0.001"
+                            value="{{$tributaryDetail['tributarydetails_paymentTotalValue']}}">
+                        </td>
+
 
                     </tr>
                     @endforeach
@@ -130,11 +138,14 @@
                 <tbody>
                     @foreach($contractPaymentDetails as $contractPaymentDetail)
                     <tr>
-                        <td>{{$contractPaymentDetail['id']}}</td>
+                        <td>{{$contractPaymentDetail['id']}}
+                            <input type="hidden" name="contractPaymentDetail_id[]" required
+                            value="{{$contractPaymentDetail['id']}}">
+                        </td>
                         <td>
-                            <select class="form-control" id="clientRazonSocial[{{$contractPaymentDetail['id']}}]" name="clientRazonSocial[]">
+                            <select class="form-control" id="contractPaymentDetail_idClient[{{$contractPaymentDetail['id']}}]" name="contractPaymentDetail_idClient[]">
                                 @foreach($razonesSociales as $razonSocial)
-                                <option value="{{$razonSocial['clientRazonSocial']}}" @if($contractPaymentDetail['clientRazonSocial'] == $razonSocial['clientRazonSocial']) selected @endif>
+                                <option value="{{$razonSocial['id']}}" @if($contractPaymentDetail['idClient'] == $razonSocial['id']) selected @endif>
                                     {{$razonSocial['clientRazonSocial']}}
                                 </option>
                                 @endforeach
@@ -152,6 +163,8 @@
 
         <br>
         <input type="hidden" id="tributaryDetailsTableLength" name="tributaryDetailsTableLength" required value="0">
+        <input type="hidden" id="contractPaymentDetailsTableLength" name="contractPaymentDetailsTableLength" required value="0">
+
         <div class="form-group row">
             <div class="col-md-3">
                 <button type="submit" class="btn btn-primary">
@@ -181,6 +194,9 @@
     const tributaryDetailsTableLength = $('#tablaTributaryDetails').bootstrapTable('getData').length;
     document.getElementById('tributaryDetailsTableLength').value = tributaryDetailsTableLength;
 
+    const contractPaymentDetailsTableLength = $('#contractPaymentDetailsTableLength').bootstrapTable('getData').length;
+    document.getElementById('contractPaymentDetailsTableLength').value = contractPaymentDetailsTableLength;
+
 
     function getPercentage(percentageInput) {
         //Entradas no validas
@@ -195,25 +211,31 @@
             let inputId = getInputId(percentageInput);
             //Input del valor asociado al porcentaje
             let valueInput = document.getElementById('tributarydetails_paymentValue[' + inputId + ']');
+            let montoTotalInput = document.getElementById('tributarydetails_paymentTotalValue[' + inputId + ']');
+            let discountInput = document.getElementById('tributarydetails_discount[' + inputId + ']');
             //Si el total supera 100
             if (getTotalPorcentaje() > 100) {
                 percentageInput.value = parseFloat(0);
                 valueInput.value = parseFloat(0);
+                montoTotalInput.value = parseFloat(0);
             }
             //Si el total es menor a 0
             else if (getTotalPorcentaje() < 0) {
                 percentageInput.value = parseFloat(0);
                 valueInput.value = parseFloat(0);
+                montoTotalInput.value = parseFloat(0);
             }
             else if (percentageInput.value == '') {
                 percentageInput.value = parseFloat(0);
                 valueInput.value = parseFloat(0);
+                montoTotalInput.value = parseFloat(0);
             }
             //Si esta todo OK
             else {
                 let montoTotal = document.getElementById('montoTotal').value;
                 let value = parseFloat(percentageInput.value)/100 * parseFloat(montoTotal);
                 valueInput.value = parseFloat(value).toFixed(3);
+                montoTotalInput.value = (parseFloat(valueInput.value) * (100 - parseFloat(discountInput.value)) / 100).toFixed(3);
             }
         }
         getTotalPorcentaje();
@@ -234,25 +256,31 @@
             let inputId = getInputId(valueInput);
             //Input del porcentaje asociado al valor
             let percentageInput = document.getElementById('tributarydetails_paymentPercentage[' + inputId + ']');
+            let montoTotalInput = document.getElementById('tributarydetails_paymentTotalValue[' + inputId + ']');
+            let discountInput = document.getElementById('tributarydetails_discount[' + inputId + ']');
             //Verificar el total
             //Si el total supera el monto total
             if (getTotalValue() > parseFloat(montoTotal)) {
                 percentageInput.value = parseFloat(0);
                 valueInput.value = parseFloat(0);
+                montoTotalInput.value = parseFloat(0);
             }
             //Si el total es menor a 0
             else if (getTotalValue() < 0) {
                 percentageInput.value = parseFloat(0);
                 valueInput.value = parseFloat(0);
+                montoTotalInput.value = parseFloat(0);
             }
             else if (valueInput.value == '') {
                 percentageInput.value = parseFloat(0);
                 valueInput.value = parseFloat(0);
+                montoTotalInput.value = parseFloat(0);
             }
             //Si esta todo OK
             else {
                 let percentage = parseFloat(valueInput.value) * 100 / parseFloat(montoTotal);
                 percentageInput.value = parseFloat(percentage).toFixed(2);
+                montoTotalInput.value = (parseFloat(valueInput.value) * (100 - parseFloat(discountInput.value)) / 100).toFixed(3);
             }
         }
         getTotalPorcentaje();
@@ -268,6 +296,52 @@
         return totalPercentage;
     }
 
+    function getMontoTotal(montoTotalInput) {
+        let inputId = getInputId(montoTotalInput);
+        let valueInput = document.getElementById('tributarydetails_paymentValue[' + inputId + ']');
+        let discountInput = document.getElementById('tributarydetails_discount[' + inputId + ']');
+        let percentageInput = document.getElementById('tributarydetails_paymentPercentage[' + inputId + ']');
+        let montoTotal = document.getElementById('montoTotal').value;
+        //Si pone valor 0 o elimina el valor
+        if (parseFloat(montoTotalInput.value) == 0 || montoTotalInput.value == '') {
+            percentageInput.value = parseFloat(0);
+            valueInput.value = parseFloat(0);
+            montoTotalInput.value = parseFloat(0);
+        }
+        //Entradas sin descuento
+        else if (parseFloat(discountInput.value) == 0) {
+            //Si es mayor al subtotal o si es menor a 0 - NO validas
+            if (parseFloat(montoTotalInput.value) > parseFloat(valueInput.value) || parseFloat(montoTotalInput.value) < 0) {
+                montoTotalInput.value = parseFloat(valueInput.value);
+            }
+            //Entrada valida
+            else {
+                //total y subtotal son iguales
+                valueInput.value = parseFloat(montoTotalInput.value);
+                let percentage = parseFloat(valueInput.value) * 100 / parseFloat(montoTotal);
+                percentageInput.value = parseFloat(percentage).toFixed(2);
+            }
+        }
+        //Entradas con descuento
+        else {
+            let montoTotalValue = parseFloat(valueInput.value) * (100 - parseFloat(discountInput.value)) / 100;
+            //Si es mayor al subtotal o si es menor a 0 - NO validas
+            if (parseFloat(montoTotalInput.value) > parseFloat(montoTotalValue) || parseFloat(montoTotalInput.value) < 0) {
+                montoTotalInput.value = parseFloat(montoTotalValue).toFixed(3);
+            }
+            //Entrada valida
+            else {
+                //total y subtotal NO son iguales
+                let valueNoDiscount = (parseFloat(montoTotalInput.value) * 100 / (100 - parseFloat(discountInput.value))).toFixed(3);
+                valueInput.value = parseFloat(valueNoDiscount);
+                let percentage = parseFloat(valueInput.value) * 100 / parseFloat(montoTotal);
+                percentageInput.value = parseFloat(percentage).toFixed(2);
+            }
+        }
+        getTotalPorcentaje();
+        getTotalValue();
+    }
+
     function getTotalValue() {
         let totalValue = 0;
         document.getElementsByName('tributarydetails_paymentValue[]').forEach(input => {
@@ -277,11 +351,6 @@
         return totalValue;
     }
 
-    function getInputId(inputElement) {
-        //Regex
-        let inputId = inputElement.id.match(/(\d+)/);
-        return inputId[0];
-    }
 
     function getDiscount(discountInput) {
         if (parseFloat(discountInput.value) > 100) {
@@ -290,7 +359,32 @@
         else if (parseFloat(discountInput.value) < 0) {
             discountInput.value = parseFloat(0);
         }
+        else if (discountInput.value == '') {
+            discountInput.value = parseFloat(0);
+        }
+        //Valores validos
+        else {
+            let inputId = getInputId(discountInput);
+            let valueInput = document.getElementById('tributarydetails_paymentValue[' + inputId + ']');
+            let montoTotal = document.getElementById('tributarydetails_paymentTotalValue[' + inputId + ']');
+            //Le quita el descuento, subtotal es el mismo q el total
+            if (parseFloat(discountInput.value) == 0) {
+                montoTotal.value = parseFloat(valueInput.value);
+            //Si agrega descuento, calcular
+            } else {
+                montoTotal.value = (parseFloat(valueInput.value) * (100 - parseFloat(discountInput.value)) / 100).toFixed(3);
+            }
+        }
     }
+
+
+    function getInputId(inputElement) {
+        //Regex
+        let inputId = inputElement.id.match(/(\d+)/);
+        return inputId[0];
+    }
+
+
 
     getTotalPorcentaje();
     getTotalValue();
